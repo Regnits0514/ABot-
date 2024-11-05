dc0d4fd9ec3b136b06acd19b5367ac7d921a4b11
Regnits0514 <168968108+Regnits0514@users.noreply.github.com>
2024-11-05 18:26:32 +0900

    아두이노 IDE 소스코드 추가

---
+++ sourcecode2.ino
@@ -0,0 +1,25 @@
+#include <Servo.h>
+
+Servo sr, sl;
+
+void setup() 
+{
+  Serial.begin(9600);
+  sr.attach(13);
+  sl.attach(12);
+}
+
+void loop() 
+{
+  if (Serial.available()) {
+    char b[2];
+    int p, r;
+
+    Serial.readBytes(b, 2);
+    p = b[0] * 3;
+    r = b[1];
+    sr.write(1500 + p - r);
+    sl.write(1500 - p - r);
+    Serial.write('1');
+  }
+}
